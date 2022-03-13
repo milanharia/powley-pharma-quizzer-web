3239c827de5acfccc4dd3908617973de6ff36dcd
milan.haria <milan.haria@morrisonsplc.co.uk>
2022-03-13 19:56:27 +0000

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,50 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  primary: "#7579E7",
+  secondary: "#9AB3F5",
+  light: "#B9FFFC",
+  // primary: {
+  //   900: '#1a365d',
+  //   800: '#153e75',
+  //   700: '#2a69ac',
+  // },
+};
+
+const fonts = {
+  body: "system-ui, sans-serif",
+  heading: "Poppins, sans-serif",
+  mono: "Menlo, monospace",
+};
+const fontSizes = {
+  xs: "0.75rem",
+  sm: "0.875rem",
+  md: "1rem",
+  lg: "1.125rem",
+  xl: "1.25rem",
+  "2xl": "1.5rem",
+  "3xl": "1.875rem",
+  "4xl": "2.25rem",
+  "5xl": "3rem",
+  "6xl": "3.75rem",
+  "7xl": "4.5rem",
+  "8xl": "6rem",
+  "9xl": "8rem",
+  title: "80px",
+};
+
+const theme = extendTheme({ colors, fonts, fontSizes });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
