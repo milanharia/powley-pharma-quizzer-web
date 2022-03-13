c2b60df5c65303d6f76bb0a328725f3cfe4401d1
milan.haria <milan.haria@morrisonsplc.co.uk>
2022-03-13 20:42:53 +0000

    Deploy gh-pages

--- _next/static/zbjpJgzHL9T5g0Sd-nFsW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/398-0acc424020ceb6de.js","static/chunks/pages/index-167be7c6ef4e4002.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
