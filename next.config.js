ae6ca88168ff07b27a7160d4ddb542a03a7af89b
milan.haria <milan.haria@morrisonsplc.co.uk>
2022-03-13 20:50:45 +0000

    fix: deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
 const nextConfig = {
   reactStrictMode: true,
-}
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/ap-grid-layout": { page: "/ap-grid-layout" },
+    };
+  },
+  assetPrefix: !debug
+    ? "https://anotherplanet-io.github.io/Next-React-Components/"
+    : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
